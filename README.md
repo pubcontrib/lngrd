55b37a3997bca1700096efac72563720510324ba
Jason Kaiser <wareification@programmer.net>
2023-09-16 21:22:16 -0500

    feat: add equals built-in

--- README.md
+++ README.md
@@ -45,4 +45,4 @@
 
     # built-ins
     @add @subtract @multiply @divide @modulo @increment @decrement @and @or @not
-    @precedes @succeeds @write
+    @precedes @succeeds @equals @write
--- src/lngrd.h
+++ src/lngrd.h
@@ -338,6 +338,7 @@ static void do_or_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_St
 static void do_not_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities);
 static void do_precedes_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities);
 static void do_succeeds_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities);
+static void do_equals_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities);
 static void do_write_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities);
 static void set_global_function(const char *name, void (*work)(lngrd_Executer *, lngrd_List *, lngrd_Stash *), lngrd_Executer *executer);
 static void set_executer_error(const char *message, lngrd_Executer *executer);
@@ -917,6 +918,7 @@ LNGRD_API void lngrd_start_executer(lngrd_Executer *executer)
     set_global_function("not", do_not_work, executer);
     set_global_function("precedes", do_precedes_work, executer);
     set_global_function("succeeds", do_succeeds_work, executer);
+    set_global_function("equals", do_equals_work, executer);
     set_global_function("write", do_write_work, executer);
 }
 
@@ -1947,6 +1949,38 @@ static void do_succeeds_work(lngrd_Executer *executer, lngrd_List *arguments, ln
     }
 
     set_executor_result(create_block(LNGRD_BLOCK_TYPE_NUMBER, create_number(LNGRD_NUMBER_LAYOUT_32_0, compare_blocks(left, right) > 0), 0), executer);
+}
+
+static void do_equals_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities)
+{
+    lngrd_SInt *capacity;
+    lngrd_Block *left, *right;
+
+    capacity = (lngrd_SInt *) peek_stash_item(capacities);
+
+    if (*capacity < 3)
+    {
+        set_executer_error("absent argument", executer);
+        return;
+    }
+
+    left = arguments->items[arguments->length - *capacity + 1];
+
+    if (left->type == LNGRD_BLOCK_TYPE_FUNCTION)
+    {
+        set_executer_error("alien argument", executer);
+        return;
+    }
+
+    right = arguments->items[arguments->length - *capacity + 2];
+
+    if (right->type == LNGRD_BLOCK_TYPE_FUNCTION)
+    {
+        set_executer_error("alien argument", executer);
+        return;
+    }
+
+    set_executor_result(create_block(LNGRD_BLOCK_TYPE_NUMBER, create_number(LNGRD_NUMBER_LAYOUT_32_0, compare_blocks(left, right) == 0), 0), executer);
 }
 
 static void do_write_work(lngrd_Executer *executer, lngrd_List *arguments, lngrd_Stash *capacities)
